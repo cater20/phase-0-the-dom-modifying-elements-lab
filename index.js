4b3389f878cf951aacf3df7da187518bafa25757
cater20 <onyangoketta@gmail.com>
2022-06-12 11:35:56 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+main.remove();
+
+//document.body.append(element);
+
+//document.getElementById("newHeader").querySelector("<h1>HI</h1>");
+//const h1 = document.querySelector("<h1>HI</h1>");
+//h1;
+const newHeader= document.createElement("h1");
+
+document.getElementById('main')
+ newHeader . id="victory"
+ //newHeader.innerHTML="is the champion"
+ 
+newHeader.innerHTML=
+"<h1>is the champion</h1>"
